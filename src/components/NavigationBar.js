2b52d8b7e4485b8223ca9cd0f54ed9688d43c00a
f-ssemwanga <dssales@gmx.com>
2023-06-26 20:44:28 +0100

    working nav bar completed and ready for styling

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,330 +1,125 @@
-
-import * as React from "react";
-
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
-
 import Box from "@mui/material/Box";
-
 import Toolbar from "@mui/material/Toolbar";
-
 import IconButton from "@mui/material/IconButton";
-
-import Typography from "@mui/material/Typography";
-
-import Menu from "@mui/material/Menu";
-
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
 import MenuIcon from "@mui/icons-material/Menu";
-
 import Container from "@mui/material/Container";
-
-import Avatar from "@mui/material/Avatar";
-
 import Button from "@mui/material/Button";
-
-import Tooltip from "@mui/material/Tooltip";
-
-import MenuItem from "@mui/material/MenuItem";
-
-import AdbIcon from "@mui/icons-material/Adb";
-
-
-
-
-const pages = ["Home", "Footprint", "Journey"];
-
-// const settings = ["Profile", "Account", "Dashboard", "Logout"];
-
-
-
+import Drawer from "@mui/material/Drawer";
+import Badge from "@mui/material/Badge";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 export const NavigationBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const navigate = useNavigate();
 
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-
-
-
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-
-    setAnchorElNav(event.currentTarget);
-
+  const handleOpenDrawer = () => {
+    setIsOpen(true);
   };
 
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-
-    setAnchorElUser(event.currentTarget);
-
+  const handleCloseDrawer = () => {
+    setIsOpen(false);
   };
-
-
-
-
-  const handleCloseNavMenu = () => {
-
-    setAnchorElNav(null);
-
-  };
-
-
-
-
-  const handleCloseUserMenu = () => {
-
-    setAnchorElUser(null);
-
-  };
-
-
-
 
   return (
-
     <AppBar position="static">
-
       <Container maxWidth="xl">
-
         <Toolbar disableGutters>
+          <Drawer anchor="top" open={isOpen} onClose={handleCloseDrawer}>
+            <List>
+              <ListItem disablePadding>
+                <ListItemButton
+                  onClick={() => {
+                    handleCloseDrawer();
+                    navigate("/");
+                  }}
+                >
+                  <ListItemText primary="Home" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton
+                  onClick={() => {
+                    handleCloseDrawer();
+                    navigate("/footprint");
+                  }}
+                >
+                  <ListItemText primary="Footprint" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton
+                  onClick={() => {
+                    handleCloseDrawer();
+                    navigate("/journey");
+                  }}
+                >
+                  <ListItemText primary="Journey" />
+                </ListItemButton>
+              </ListItem>
+            </List>
+          </Drawer>
 
-          <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
-
-          <Typography
-
-            variant="h6"
-
-            noWrap
-
-            component="a"
-
-            href="/"
-
+          <Box
             sx={{
-
-              mr: 2,
-
-              display: { xs: "none", md: "flex" },
-
-              fontFamily: "monospace",
-
-              fontWeight: 700,
-
-              letterSpacing: ".3rem",
-
-              color: "inherit",
-
-              textDecoration: "none",
-
-            }}
-
-          >
-
-            ECO TRACKER
-
-          </Typography>
-
-
-
-
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-
-            <IconButton
-
-              size="large"
-
-              aria-label="account of current user"
-
-              aria-controls="menu-appbar"
-
-              aria-haspopup="true"
-
-              onClick={handleOpenNavMenu}
-
-              color="inherit"
-
-            >
-
-              <MenuIcon />
-
-            </IconButton>
-
-            <Menu
-
-              id="menu-appbar"
-
-              anchorEl={anchorElNav}
-
-              anchorOrigin={{
-
-                vertical: "bottom",
-
-                horizontal: "left",
-
-              }}
-
-              keepMounted
-
-              transformOrigin={{
-
-                vertical: "top",
-
-                horizontal: "left",
-
-              }}
-
-              open={Boolean(anchorElNav)}
-
-              onClose={handleCloseNavMenu}
-
-              sx={{
-
-                display: { xs: "block", md: "none" },
-
-              }}
-
-            >
-
-              {pages.map((page) => (
-
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-
-                  <Typography textAlign="center">{page}</Typography>
-
-                </MenuItem>
-
-              ))}
-
-            </Menu>
-
-          </Box>
-
-          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
-
-          <Typography
-
-            variant="h5"
-
-            noWrap
-
-            component="a"
-
-            href=""
-
-            sx={{
-
-              mr: 2,
-
-              display: { xs: "flex", md: "none" },
-
               flexGrow: 1,
-
-              fontFamily: "monospace",
-
-              fontWeight: 700,
-
-              letterSpacing: ".3rem",
-
-              color: "inherit",
-
-              textDecoration: "none",
-
+              display: { xs: "flex", md: "none" },
+              justifyContent: "space-between",
             }}
-
           >
-
-            ECO TRACKER
-
-          </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-
-            {pages.map((page) => (
-
-              <Button
-
-                key={page}
-
-                onClick={handleCloseNavMenu}
-
-                sx={{ my: 2, color: "white", display: "block" }}
-
-              >
-
-                {page}
-
-              </Button>
-
-            ))}
-
+            <IconButton size="large" onClick={handleOpenDrawer} color="inherit">
+              <MenuIcon />
+            </IconButton>
           </Box>
-
-
-
-
-          <Box sx={{ flexGrow: 0 }}>
-
-            {/* <Tooltip title="Open settings">
-
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-
-              </IconButton>
-
-            </Tooltip> */}
-
-            <Menu
-
-              sx={{ mt: "45px" }}
-
-              id="menu-appbar"
-
-              anchorEl={anchorElUser}
-
-              anchorOrigin={{
-
-                vertical: "top",
-
-                horizontal: "right",
-
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", md: "flex" },
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
               }}
-
-              keepMounted
-
-              transformOrigin={{
-
-                vertical: "top",
-
-                horizontal: "right",
-
-              }}
-
-              open={Boolean(anchorElUser)}
-
-              onClose={handleCloseUserMenu}
-
             >
-
-              {/* {settings.map((setting) => (
-
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-
-                  <Typography textAlign="center">{setting}</Typography>
-
-                </MenuItem>
-
-              ))} */}
-
-            </Menu>
-
+              <Button
+                onClick={() => {
+                  navigate("/");
+                }}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                Home
+              </Button>
+              <Button
+                onClick={() => {
+                  navigate("/footprint");
+                }}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                Footprint
+              </Button>
+              <Button
+                onClick={() => {
+                  navigate("/journey");
+                }}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                Journey
+              </Button>
+            </Box>
           </Box>
-
         </Toolbar>
-
       </Container>
-
     </AppBar>
-
   );
-
 };
