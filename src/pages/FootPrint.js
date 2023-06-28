42fdf471bb1b4498b4db1f35548cebbb9a3f7034
Viktoria <vicky1234555617@gmail.com>
2023-06-28 20:44:32 +0100

    ended try to do API call

--- src/pages/FootPrint.js
+++ src/pages/FootPrint.js
@@ -1,28 +1,36 @@
 import { Stack } from "@mui/material";
 import { CarbonResult } from "../components/CarbonResult";
 import { CarbonForm } from "../components/CarbonForm";
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import axios from "axios";
 
 export const FootPrint = () => {
   const [ {fuelType, distance},setValues]=useState("{}")
- console.log(distance)
- console.log(fuelType)
- const url = `https://carbonfootprint1.p.rapidapi.com/CarbonFootprintFromCarTravel,
- params: {
-   distance: ${distance},
-   vehicle: ${fuelType}
- } , 
- headers: {
-  'X-RapidAPI-Key': 'e279ab2909msh66df9814db3584cp11be92jsneb2b0f95220c',
-  'X-RapidAPI-Host': 'carbonfootprint1.p.rapidapi.com'
-} `
+ //console.log(distance)
+ //console.log(fuelType) 
 
-const fetchDate = async () => {
- const [{data}] = await axios.get(url)
- console.log(data)
-}
-fetchDate()
+useEffect(() => {
+
+}, [fuelType, distance])  
+
+
+const fetchData = async () => {
+    const {data} = await axios.get( 
+      "https://carbonfootprint1.p.rapidapi.com/CarbonFootprintFromCarTravel",
+    {
+      params: {
+      distance: `${distance}`,
+      vehicle: `${fuelType}`
+      } , 
+      headers: {
+      'X-RapidAPI-Key': 'e279ab2909msh66df9814db3584cp11be92jsneb2b0f95220c',
+      'X-RapidAPI-Host': 'carbonfootprint1.p.rapidapi.com'
+     },
+     }
+   );
+    }
+
+ fetchData()
 
   return (
     <Stack>
