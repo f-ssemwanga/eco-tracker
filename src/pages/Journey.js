53e3cbb50e136ffc35f8d57279a4345e06751334
N20-Dev <lukedcr@outlook.com>
2023-07-05 19:44:21 +0100

    Updated Journey Page

--- src/pages/Journey.js
+++ src/pages/Journey.js
@@ -1,3 +1,31 @@
+import React, { useState, useEffect } from "react";
+import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+import JourneyCard from "../components/JourneyCard";
+
 export const Journey = () => {
-  return <div>Journey</div>;
+  const [journeys, setJourneys] = useState([]);
+
+  useEffect(() => {
+    const savedJourneys = JSON.parse(localStorage.getItem("journeys")) || [];
+    setJourneys(savedJourneys);
+  }, []);
+
+  const handleDelete = (id) => {
+    const newJourneys = journeys.filter((journey) => journey.id !== id);
+    setJourneys(newJourneys);
+    localStorage.setItem("journeys", JSON.stringify(newJourneys));
+  };
+
+  return (
+    <Stack spacing={3}>
+      <Grid container spacing={3}>
+        {journeys.map((journey) => (
+          <Grid item xs={12} sm={6} md={4} key={journey.id}>
+            <JourneyCard journey={journey} onDelete={handleDelete} />
+          </Grid>
+        ))}
+      </Grid>
+    </Stack>
+  );
 };
